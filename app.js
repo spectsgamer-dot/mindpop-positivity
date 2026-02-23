961e8ddacf404bc88af1aab56c71a6286c6e37c7
spectsgamer-dot <spectsgamer@gmail.com>
2026-02-24 00:22:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -592,28 +592,6 @@ function restartAssessment() {
   location.reload();
 }
 
-function sendToBackend() {
-  const endpoint = "xyxz/exedc";
-
-  fetch(endpoint, {
-    method: "POST",
-    mode: "no-cors",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(sessionState)
-  })
-  .then(() => console.log("Data sent"))
-  .catch(err => console.error("Backend error:", err));
-}
-function fetchData() {
-  const endpoint = "xyxz/exedc";
-
-  fetch(endpoint)
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
-
 // ---------------- START ----------------
 
 if (sessionState.completedTests.length > 0) {
